8e921aa304584911053f17969c21948c9f9b3841
arendon7 <102555888+arendon7@users.noreply.github.com>
2026-08-09 19:19:41 -0500

    M33.0 integra profundización jurídica final

--- m33_wave3_runtime.py
+++ m33_wave3_runtime.py
@@ -7,6 +7,7 @@ from m33_consumer_release_polish import finalize_consumer_release_polish
 from m33_debt_layout_polish import finalize_debt_layout_polish
 from m33_debt_legal_finalize import finalize_debt_specs
 from m33_debt_release_polish import finalize_debt_release_polish
+from m33_depth_polish import finalize_depth_polish
 from m33_habeas_legal_finalize import finalize_habeas_specs
 from m33_health_compat_polish import finalize_health_compat_polish
 from m33_health_legal_finalize import finalize_health_specs
@@ -44,21 +45,24 @@ def document_specs_m33_all(case_id, code, answers, result, product, generated_at
         return _polish_habeas_client_presentation(habeas_specs)
     if code == "CO-CD-003":
         consumer_specs = finalize_consumer_specs(specs, answers, result)
-        return finalize_consumer_release_polish(consumer_specs)
+        consumer_specs = finalize_consumer_release_polish(consumer_specs)
+        return finalize_depth_polish(code, consumer_specs, answers, result)
     if code == "CO-CD-004":
         debt_specs = finalize_debt_specs(specs, answers, result)
         debt_specs = finalize_debt_release_polish(debt_specs, answers, result)
         return finalize_debt_layout_polish(debt_specs, answers, result)
     if code == "CO-SA-001":
         health_specs = finalize_health_specs(specs, answers, result)
-        return finalize_health_compat_polish(health_specs, answers)
+        health_specs = finalize_health_compat_polish(health_specs, answers)
+        return finalize_depth_polish(code, health_specs, answers, result)
     if code == "CO-TR-001":
         sast_specs = finalize_sast_specs(specs, answers, result)
         sast_specs = finalize_sast_compat_polish(sast_specs)
         return finalize_sast_release_polish(sast_specs)
     if code == "CO-TR-002":
         traffic_specs = finalize_traffic_specs(specs, answers, result)
-        return finalize_traffic_release_polish(traffic_specs, answers)
+        traffic_specs = finalize_traffic_release_polish(traffic_specs, answers)
+        return finalize_depth_polish(code, traffic_specs, answers, result)
     if code not in WAVE3_CODES:
         return specs
     return [_finalize_spec(code, answers, spec) for spec in specs]
