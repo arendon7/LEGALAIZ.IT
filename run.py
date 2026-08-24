8fc91be8a2cf84e5659dc0ffb883d402f100ce6e
arendon7 <102555888+arendon7@users.noreply.github.com>
2026-08-23 22:02:35 -0500

    feat(m36.0): activate fulfillment intake handler

--- run.py
+++ run.py
@@ -22,7 +22,8 @@ from legalai_platform.runtime_registry import *  # noqa: F401,F403,E402
 from legalai_platform.release_metadata import RELEASE_NAME, PUBLIC_DEMO_MODE, MILESTONE  # noqa: E402
 from legalai_platform.application_services import *  # noqa: F401,F403,E402
 import legalai_platform.application_services as _application_services  # noqa: E402
-from legalai_platform.http_handler_m35_3 import Handler  # noqa: E402
+from legalai_platform.http_handler_m36_0 import Handler  # noqa: E402
+# from legalai_platform.http_handler_m35_3 import Handler  # compatibility marker
 # from legalai_platform.http_handler_m35_2 import Handler  # compatibility marker
 # from legalai_platform.http_handler_m35_1 import Handler  # compatibility marker
 # from legalai_platform.http_handler_m35_0 import Handler  # compatibility marker
@@ -54,6 +55,7 @@ from legalai_platform.http_handler_m35_3 import Handler  # noqa: E402
 # m35-1-fulfillment-context-offer
 # m35-2-commerce-case-traceability
 # m35-3-case-activation-purchase-confirmation
+# m36-0-fulfillment-intake-bridge
 # LEGAL_ALLOW_DEMO_ACCOUNTS
 # LEGAL_BOOTSTRAP_ADMIN_EMAIL
 # UPDATE users SET active=0 WHERE lower(email) LIKE '%@demo.legalaiz.it'
